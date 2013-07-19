15684aacf30859ebedc5dcd220b108ac1409d50b
kSaMi <khemsisami@gmail.com>
2013-07-19 22:30:51 +0200

    Merge branch 'master' of https://github.com/agrubi/bored_minutes
    Conflicts:
    	app/view/email/RecipientField.js

--- app/view/email/RecipientField.js
+++ app/view/email/RecipientField.js
@@ -20,7 +20,7 @@ Ext.define('BoredMinutes.view.email.RecipientField', {
             },
             {
                 xtype:'button',
-                text:'remove',
+                iconCls:'delete',
 
                 action:'recipientRemove'
             }
