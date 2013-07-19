204bb74cf7ece1951d0e52cd093d14460c04e04f
gostbuster <jeremyescolano@gmail.com>
2013-07-19 20:34:47 +0200

    commit new views

--- app/view/picture/PictureCanvas.js
+++ app/view/picture/PictureCanvas.js
@@ -2,7 +2,7 @@ Ext.define('BoredMinutes.view.picture.PictureCanvas', {
     extend: 'Ext.Component',
     xtype:'picturecanvas',
     config:{
-           style:'background:green'
+
 
 
 
@@ -12,13 +12,32 @@ Ext.define('BoredMinutes.view.picture.PictureCanvas', {
         var items = [
             {
                 reference: 'canvas',
-                tag: 'canvas'
+                tag: 'canvas',
+                id:'ImgCanvas'
             }
         ];
 
 
 
         return items;
+    },
+
+    initialize:function() {
+    this.callParent(arguments);
+
+        Ext.Viewport.on('resize', 'recomputeSize', this);
+
+    },
+
+
+    recomputeSize:function(){
+        var height=this.element.getHeight();
+        var width=this.element.getWidth();
+        this.canvas.setHeight(height);
+        this.canvas.setWidth(width);
+
     }
+
+
 
 });
