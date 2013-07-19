d5b3a8e76dea0b5baf6f6e775ab44284a05e664b
gostbuster <jeremyescolano@gmail.com>
2013-07-19 22:41:30 +0200

    commit new views

--- app/controller/Email.js
+++ app/controller/Email.js
@@ -2,8 +2,25 @@ Ext.define('BoredMinutes.controller.Email', {
     extend: 'Ext.app.Controller',
 
     config: {
+        refs: {
+            emailPanel:'emailwrite'
+        },
+        control: {
+            emailPanel:{
+                sendEmail:'sendEmail'
+            }
+        }
 
 
+    },
+
+
+
+
+
+    sendEmail:function(emails){
+        console.log('controller send email');
+        console.log(emails);
     }
 
 
